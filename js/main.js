e678059358bddcc7b763735aa1d9f8760da5471f
mrpv13 <mrpv13@gmail.com>
2017-11-05 23:37:47 -0600

    Almost done
    need to fix pagination links once searched

---
+++ js/main.js
@@ -0,0 +1,101 @@
+let searchKey = "";
+let studentTotal = $('.student-list li').length;
+
+//adds or resets pagination based on student count
+const pagination = () => {
+    const pagesTotal = Math.ceil(studentTotal / 10);
+    
+    //adds pagination div if does not exist, else clears li elements
+    if($('.pagination').length == 0){
+       $('.page').append("<div class=\"pagination\"><ul></ul></div>");        
+    }else{
+        $('.pagination ul').empty();
+    }
+    
+    //adds li elements based on how many students total
+    for (let i = 1; i <= pagesTotal; i++){
+            $('.pagination ul').append("<li><a href=\"#\">" + i + "</a></li>");
+        }
+        
+    $('.pagination ul li:first a').addClass('active');
+}
+
+const showStudents = (page) => {
+    //resets pagination
+    if(page == 1){
+    $('.pagination ul li a[class="active"]').removeClass("active");
+    $('.pagination ul li:first a').addClass("active"); 
+    }
+    
+    console.log(page);
+
+    //if key is empty (no search) show 10 at a time, else uses searchKey to show for search functionality
+    if (searchKey == ""){
+        $('.student-list li').each(function(index, value){
+            if ((index + 1) <= (page*10) && (index + 1) >= ((page*10)-9)){
+                $(value).show();
+            }else{
+                $(value).hide();
+            }
+        });
+
+    }else{
+        $('.student-list li').each(function(index, value){
+            if(($(value).find('h3').text()).includes(searchKey)){
+                if ((index + 1) <= (page*10) && (index + 1) >= ((page*10)-9)){
+                    $(value).show();
+                }else{
+                    $(value).hide();
+                }
+            }else{
+                $(value).hide();                       
+            }
+        });
+    }
+}
+
+const search = () => {
+    searchKey = $('#input-search').val();
+    if (searchKey == ""){
+        studentTotal = $('.student-list li').length;
+        //console.log(studentTotal);
+        pagination();
+        showStudents(1);
+        return;   
+    }
+
+    studentTotal = 0;
+    $('.student-list li').each(function(index, value){
+            if(($(value).find('h3').text()).includes(searchKey)){
+                studentTotal++;
+            }
+    });
+    
+    pagination();
+    showStudents(1);
+}
+
+// Methods to run when page first loads
+const initPage = () => {
+    pagination();
+    showStudents(1);
+    
+    //add search
+    $('.page-header').append("<div class=\"student-search\"><input id=\"input-search\" placeholder=\"Search for students\"><button id=\"search-button\">Search</button></div>");
+}
+
+//method call to load page
+initPage();
+
+//adds showStudents function to each pagination link
+$('.pagination a').on('click', (event) => {
+    $('.pagination ul li a[class="active"]').removeClass("active");
+    $(event.target).addClass("active");
+    showStudents($(event.target).text());
+});
+
+
+//adds search function to search button
+$('#search-button').on('click', (event) => {
+    search();
+});
